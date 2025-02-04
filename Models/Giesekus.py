211eb24c61b455586fd2d6011abdebb3f30381ee
hifabian <49561468+hifabian@users.noreply.github.com>
2025-02-04 10:23:25 +0900

    Add lower-convected Giesekus model
    We add the lower-convected Giesekus model. Note that this is equivalent
    to the upper-convected Giesekus model in planar incompressible flows.
    Only the 2nd normal stress difference differs (obeying N2_A = -N1-N2).

--- Models/Giesekus.py
+++ Models/Giesekus.py
@@ -13,3 +13,17 @@ class Giesekus(ConstitutiveModel):
     def stress_tensor(A, gradU, Wi, β, α):
         return (1-β) / Wi * (A - np.eye(*A.shape)) \
             + β * (gradU+gradU.transpose())
+
+
+class Giesekus_A(ConstitutiveModel):
+    name = "Giesekus A"
+
+    @staticmethod
+    def equation(A, gradU, Wi, β, α):
+        return ConstitutiveModel.covariant_derivative(gradU, A) \
+            + ( (1-α)*np.eye(*A.shape)+α*A ) @ ( A - np.eye(*A.shape) ) / Wi
+
+    @staticmethod
+    def stress_tensor(A, gradU, Wi, β, α):
+        return (1-β) / Wi * (np.eye(*A.shape)- A) \
+            + β * (gradU+gradU.transpose())
